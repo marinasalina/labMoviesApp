416ab2c33f358a21030401321d6c529504cd9734
Marina19761107 <marinasal1107@gmail.com>
2026-06-09 22:51:26 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -19,7 +19,7 @@ const styles = {
   },
 };
 
-const MovieListPage: React.FC = () => {
+const MovieListPage: FC = () => {
   const [movies, setMovies] = useState<BaseMovieProps[]>([]);
   const [titleFilter, setTitleFilter] = useState("");
   const [genreFilter, setGenreFilter] = useState("0");
@@ -38,6 +38,13 @@ const MovieListPage: React.FC = () => {
   const handleChange = (type: FilterOption, value: string) => {
     if (type === "title") setTitleFilter(value);
     else setGenreFilter(value);
+  };
+
+  const addToFavourites = (movieId: number) => {
+    const updatedMovies = movies.map((m: BaseMovieProps) =>
+      m.id === movieId ? { ...m, favourite: true } : m,
+    );
+    setMovies(updatedMovies);
   };
 
   useEffect(() => {
@@ -62,7 +69,10 @@ const MovieListPage: React.FC = () => {
           <Header title={"Home Page"} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList movies={displayedMovies}></MovieList>
+          <MovieList
+            movies={displayedMovies}
+            selectFavourite={addToFavourites}
+          />
         </Grid>
       </Grid>
       <Fab
--- src/type/interfaces.ts
+++ src/type/interfaces.ts
@@ -19,7 +19,9 @@ export interface BaseMovieProps {
 }
 export interface BaseMovieListProps {
   movies: BaseMovieProps[];
+  selectFavourite: (movieId: number) => void; //add this
 }
+
 export interface MovieDetailsProps extends BaseMovieProps {
   genres: {
     id: number;
