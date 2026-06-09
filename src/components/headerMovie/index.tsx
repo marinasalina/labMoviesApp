cf9b6cb6704ee8afba3a5d156784bda52cd4d521
Marina19761107 <marinasal1107@gmail.com>
2026-06-09 23:39:08 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -5,6 +5,8 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { MovieDetailsProps } from "../../types/interfaces";
 
 const styles = {
@@ -18,6 +20,14 @@ const styles = {
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
+  // Read favourites
+  const raw = JSON.parse(localStorage.getItem("favourites") || "[]");
+
+  // Support both: [1,2,3] and [{id:1}, {id:2}]
+  const favouriteIds = raw.map((m: any) => (typeof m === "number" ? m : m.id));
+
+  const isFavourite = favouriteIds.includes(movie.id);
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
@@ -25,14 +35,26 @@ const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
       </IconButton>
 
       <Typography variant="h4" component="h3">
-        {movie.title}
-        {"   "}
+        {/* If the movie is in favourites, show the favourite icon */}
+        {isFavourite && (
+          <Avatar
+            sx={{
+              backgroundColor: "red",
+              display: "inline-flex",
+              marginLeft: "10px",
+            }}
+          >
+            <FavoriteIcon />
+          </Avatar>
+        )}
+        {movie.title}{" "}
         <a href={movie.homepage}>
           <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
-        <span>{`${movie.tagline}`} </span>
+        <span>{movie.tagline}</span>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
