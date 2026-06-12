dd01d2a47a7f4de4e3a719fd54d2b3baff2ddca2
Marina19761107 <marinasal1107@gmail.com>
2026-06-12 14:36:03 +0100

    Show movie review excerpts; Refactor Details page.

--- src/type/interfaces.ts
+++ src/type/interfaces.ts
@@ -53,3 +53,8 @@ export type FilterOption = "title" | "genre";
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
   title: string;
 }
+export interface Review {
+  id: string;
+  content: string;
+  author: string;
+}
